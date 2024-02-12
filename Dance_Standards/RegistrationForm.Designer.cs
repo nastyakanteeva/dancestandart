a4214fe2eb903a98ac00f6bff29e9b17c8f20788
nastyakanteeva <117898518+nastyakanteeva@users.noreply.github.com>
2024-02-12 09:38:32 +0300

    Add files via upload

---
+++ Dance_Standards/RegistrationForm.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Dance_Standards
+{
+    partial class RegistrationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.passwordPovTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.fioTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(284, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 51);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Зарегистрироваться";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(297, 279);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(230, 22);
+            this.passwordTextBox.TabIndex = 12;
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Location = new System.Drawing.Point(297, 223);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(230, 22);
+            this.loginTextBox.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(348, 465);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 22);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Авторизация";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(183, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Пароль";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(202, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Логин";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(249, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 46);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Регистрация";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // passwordPovTextBox
+            // 
+            this.passwordPovTextBox.Location = new System.Drawing.Point(297, 336);
+            this.passwordPovTextBox.Name = "passwordPovTextBox";
+            this.passwordPovTextBox.Size = new System.Drawing.Size(230, 22);
+            this.passwordPovTextBox.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(40, 326);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(222, 29);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Повторите пароль";
+            // 
+            // fioTextBox
+            // 
+            this.fioTextBox.Location = new System.Drawing.Point(297, 171);
+            this.fioTextBox.Name = "fioTextBox";
+            this.fioTextBox.Size = new System.Drawing.Size(230, 22);
+            this.fioTextBox.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(217, 161);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 29);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "ФИО";
+            // 
+            // RegistrationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 569);
+            this.Controls.Add(this.fioTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.passwordPovTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "RegistrationForm";
+            this.Text = "RegistrationForm";
+            this.Load += new System.EventHandler(this.RegistrationForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox passwordPovTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox fioTextBox;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ Dance_Standards/RegistrationForm.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Aspose.Cells;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Dance_Standards
+{
+    public partial class RegistrationForm : Form
+    {
+        public RegistrationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string fio = fioTextBox.Text.ToString();
+                string login = loginTextBox.Text.ToString();
+                string password = passwordTextBox.Text.ToString();
+                string passwordPov = passwordPovTextBox.Text.ToString();
+
+                if (Proverka(login, password)) {
+                    if (ProverkaPassword(password, passwordPov))
+                    {
+                        Workbook workbook = new Workbook("D:\\AutReg.xlsx");
+
+                        Worksheet worksheet = workbook.Worksheets[0];
+
+                        int worksheetRowCount1 = worksheet.Cells.MaxDataRow;
+                        int worksheetRowCount2 = worksheet.Cells.MaxDataRow;
+                        int worksheetRowCount3 = worksheet.Cells.MaxDataRow;
+
+
+                        Cell FIO = worksheet.Cells[worksheetRowCount1 + 1, 0];
+                        FIO.PutValue(fio);
+
+                        Cell Login = worksheet.Cells[worksheetRowCount2 + 1, 1];
+                        Login.PutValue(login);
+
+                        Cell Password = worksheet.Cells[worksheetRowCount3 + 1, 2];
+                        Password.PutValue(password);
+
+                        worksheet.AutoFitRows();
+
+                        workbook.Save("D:\\AutReg.xlsx", SaveFormat.Xlsx);
+
+                        worksheetRowCount1 = 0;
+                        worksheetRowCount2 = 0;
+                        worksheetRowCount3 = 0;
+
+                        MessageBox.Show("Регистрация успешна", "Успех");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Пароли не совпадают", "Ошибка");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Такой пользователь уже есть", "Ошибка");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка");
+            }
+        }
+
+        public bool ProverkaPassword (string password, string passwordPov)
+        {
+            if(password == passwordPov)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Proverka(string login, string password)
+        {
+            Workbook workbook = new Workbook("D:\\AutReg.xlsx");
+
+            Worksheet worksheet = workbook.Worksheets[0];
+
+            int worksheetRowCount = worksheet.Cells.MaxDataRow;
+            bool proverka = false;
+
+            for (int i = 0; i < worksheetRowCount + 1; i++)
+            {
+                if (Convert.ToString(worksheet.Cells[i, 1].Value) == login)
+                {
+                    if (Convert.ToString(worksheet.Cells[i, 2].Value) == password)
+                    {
+                        proverka = true;
+                        break;
+                    }
+                }
+            }
+
+            if (proverka)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            AuthorizationForm form = new AuthorizationForm();
+            form.Show();
+            this.Close();
+        }
+
+        private void RegistrationForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
